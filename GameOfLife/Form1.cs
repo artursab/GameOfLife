f95765995f0a116eca210cb79be8a636236c0c29
artursab <arturs.belavskis@gmail.com>
2023-10-22 17:52:45 +0200

    generation

--- GameOfLife/Form1.cs
+++ GameOfLife/Form1.cs
@@ -13,7 +13,7 @@ namespace GameOfLife
 {
     public partial class Form1 : Form
     {
-
+        private int currentGeneration = 0;
         private Graphics graphics;
         private int resolution;
         private bool[,] field;
@@ -30,6 +30,9 @@ namespace GameOfLife
         {
             if (timer1.Enabled)
                 return;
+
+            currentGeneration = 0;
+            Text = $"Generation {currentGeneration}";
 
             nudDensity.Enabled = false;
             nudResolution.Enabled = false;
@@ -61,27 +64,56 @@ namespace GameOfLife
         {
             graphics.Clear(Color.Black);
 
+            var newfield = new bool[cols, rows];
+
             for (int x = 0; x < cols; x++)
             {
                 for (int y = 0; y < rows; y++)
                 {
-                    if (field[x,y])
-                    {
-                        graphics.FillRectangle(Brushes.Crimson, x * resolution, y * resolution, resolution,resolution);
-                    }
+                    var neighbourCount = CountNeighbours(x, y);
+                    var hasLife = field[x, y];
+
+                    if (!hasLife && neighbourCount == 3)
+                        newfield[x, y] = true;
+
+                    else if (hasLife && (neighbourCount < 2 || neighbourCount > 3))
+                        newfield[x, y] = false;
+
+                    else
+                        newfield[x,y] = field[x, y];
+
+                    if (hasLife)
+                        graphics.FillRectangle(Brushes.Crimson, x * resolution, y * resolution, resolution, resolution);
                 }
             }
 
-            Random random = new Random();
-            for (int x = 0; x < cols; x++)
-            {
-                for (int y = 0; y < rows; y++)
-                {
-                    field[x, y] = random.Next((int)nudDensity.Value) == 0;
-                }
-            }
-
+            field = newfield;
             pictureBox1.Refresh();
+            Text = $"Genration {++currentGeneration}";
+        }
+
+        private int CountNeighbours(int x , int y)
+        {
+            int count = 0;
+
+            for (int i = -1; i < 2; i++)
+            {
+                for (int j = -1; j < 2; j++)
+                {
+                    var col = (x + i + cols) % cols;
+                    var row = (y + j + rows) % rows;
+
+                    var isSelfChecking = col == x && row == y;
+                    var hasLife = field[col, row];
+
+                    if (hasLife)
+                        count++;
+
+                    
+                }
+            }
+
+            return count;
         }
 
         private void StopGame()
@@ -107,6 +139,40 @@ namespace GameOfLife
         private void bStop_Click(object sender, EventArgs e)
         {
             StopGame();
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!timer1.Enabled)
+                return;
+
+            if (e.Button == MouseButtons.Left)
+            {
+                var x = e.Location.X / resolution;
+                var y = e.Location.Y / resolution;
+                var validationPassed = ValidateMousePosition(x, y);
+                if (validationPassed)
+                    field[x, y] = true;
+            }
+
+            if (e.Button == MouseButtons.Right)
+            {
+                var x = e.Location.X / resolution;
+                var y = e.Location.Y / resolution;
+                var validationPassed = ValidateMousePosition(x, y);
+                if (validationPassed)
+                    field[x, y] = false;
+            }
+        }
+
+        private bool ValidateMousePosition(int x, int y)
+        {
+            return x >= 0 && y >= 0 && x < cols && y < rows;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Text = $"Generation {currentGeneration}";
         }
     }
 }
