5a16aded731dd7426002d91be85df53dc570fcdf
artursab <arturs.belavskis@gmail.com>
2023-10-24 11:40:44 +0200

    consoleapp

---
+++ ConsoleLife/GameEngine.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleLife
+{
+    public class GameEngine
+    {
+        public uint CurrentGeneration { get; private set; }
+        private bool[,] field;
+        private int cols;
+        private int rows;
+
+
+        public GameEngine(int rows, int cols, int density)
+        {
+            this.rows = rows;
+            this.cols = cols;
+            field = new bool[cols, rows];
+            Random random = new Random();
+            for (int x = 0; x < cols; x++)
+            {
+                for (int y = 0; y < rows; y++)
+                {
+                    field[x, y] = random.Next(density) == 0;
+                }
+            }
+        }
+
+        private int CountNeighbours(int x, int y)
+        {
+            int count = 0;
+
+            for (int i = -1; i < 2; i++)
+            {
+                for (int j = -1; j < 2; j++)
+                {
+                    var col = (x + i + cols) % cols;
+                    var row = (y + j + rows) % rows;
+
+                    var isSelfChecking = col == x && row == y;
+                    var hasLife = field[col, row];
+
+                    if (hasLife && !isSelfChecking)
+                        count++;
+
+
+                }
+            }
+
+            return count;
+        }
+
+        public void NextGeneration()
+        {
+
+
+            var newfield = new bool[cols, rows];
+
+            for (int x = 0; x < cols; x++)
+            {
+                for (int y = 0; y < rows; y++)
+                {
+                    var neighbourCount = CountNeighbours(x, y);
+                    var hasLife = field[x, y];
+
+                    if (!hasLife && neighbourCount == 3)
+                        newfield[x, y] = true;
+
+                    else if (hasLife && (neighbourCount < 2 || neighbourCount > 3))
+                        newfield[x, y] = false;
+                    else
+                        newfield[x, y] = field[x, y];
+
+
+                }
+            }
+
+            field = newfield;
+            CurrentGeneration++;
+        }
+
+        public bool[,] GetCurrentGeneration()
+        {
+            var result = new bool[cols, rows];
+            for (int x = 0; x < cols; x++)
+            {
+                for (int y = 0; y < rows; y++)
+                {
+                    result[x, y] = field[x, y];
+                }
+            }
+
+            return result;
+        }
+
+        private bool ValidateCellPosition(int x, int y)
+        {
+            return x >= 0 && y >= 0 && x < cols && y < rows;
+        }
+
+        private void UpdateCell(int x, int y, bool state)
+        {
+            if (ValidateCellPosition(x, y))
+                field[x, y] = state;
+        }
+
+        public void AddCell(int x, int y)
+        {
+            UpdateCell(x, y, state: true);
+        }
+
+        public void RemoveCell(int x, int y)
+        {
+            UpdateCell(x, y, state: false);
+        }
+
+    }
+}
---
+++ ConsoleLife/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsoleLife
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+            Console.SetCursorPosition(0, 0);
+            Console.ReadLine();
+
+            var gameEngine = new GameEngine
+            (
+                rows: 168,
+                cols: 630,
+                density: 2
+            );
+
+            while (true)
+            {
+                Console.Title = gameEngine.CurrentGeneration.ToString();
+
+                var field = gameEngine.GetCurrentGeneration();
+
+                for (int y = 0; y < field.GetLength(1); y++)
+                {
+                    var str = new char[field.GetLength(0)];
+
+                    for (int x = 0; x < field.GetLength(0); x++)
+                    {
+                        if (field[x, y])
+                            str[x] = '#';
+
+                        else
+                            str[x] = ' ';
+                    }
+                    Console.WriteLine(str);
+                }
+            }
+        }
+    }
+}
